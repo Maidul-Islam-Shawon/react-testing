62e548b4993096d25551acc95ca6f17ab0143440
Maidul-Islam-Shawon <mi_shawon@yahoo.com>
2020-08-20 17:36:57 +0100

    Redux devtools extension added.

--- src/CreateStore.js
+++ src/CreateStore.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import RootReducer from "./Reducers/RootReducer";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 export const middlewares = [thunk];
 
-export const createStoreWithMiddlware = applyMiddleware(...middlewares)(
-  createStore
-);
+export const createStoreWithMiddlware = composeWithDevTools(
+  applyMiddleware(...middlewares)
+)(createStore);
 
 export const store = createStoreWithMiddlware(RootReducer);
